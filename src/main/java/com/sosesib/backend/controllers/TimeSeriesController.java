e5050ac0227686f907e8c17d63eee1f11f28aa1b
maroun ayle <marounayle@gmail.com>
2020-12-30 18:13:42 +0200

    Completed CSV export function

--- src/main/java/com/sosesib/backend/controllers/TimeSeriesController.java
+++ src/main/java/com/sosesib/backend/controllers/TimeSeriesController.java
@@ -1,23 +1,29 @@
 package com.sosesib.backend.controllers;
 
+import com.sosesib.backend.models.Aggregation;
 import com.sosesib.backend.models.entities.TimeSeries;
 import com.sosesib.backend.models.requests.TimeSeriesRequestWithAggregations;
 import com.sosesib.backend.models.response.generators.SOSResponseGenerator;
 import com.sosesib.backend.models.responses.SOSResponse;
 import com.sosesib.backend.models.responses.TimeSeriesResponseWithAggregations;
 import com.sosesib.backend.services.TimeSeriesService;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.io.ICsvWriter;
 import org.supercsv.prefs.CsvPreference;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping("/ts")
@@ -39,28 +45,65 @@ public class TimeSeriesController {
     }
 
     @GetMapping("/export")
-    public void exportToCSV(HttpServletResponse response) throws IOException {
-        response.setContentType("text/csv");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        String currentDateTime = dateFormatter.format(new Date());
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=TS_Sens1_" + currentDateTime + ".csv";
-        response.setHeader(headerKey, headerValue);
+    public List<File> generateCSVFiles(TimeSeriesRequestWithAggregations request) throws IOException {
+        List<TimeSeries> original_series = timeSeriesService.getTimeSeries(request);
+        Map<String,List<Aggregation<Double>>> aggregations = timeSeriesService.getAggregations(request);
 
-        List<TimeSeries> dummy = timeSeriesService.getAllSeriesBySensorId(1);
-
-        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
-        String[] csvHeader = {"measurementId", "sensorId", "measurementDate", "measurementValue"};
-        String[] nameMapping = {"id", "email", "fullName", "roles", "enabled"};
-
-        csvWriter.writeHeader(csvHeader);
-
-        for (TimeSeries ts : dummy) {
-            csvWriter.write(ts, csvHeader);
+        for(TimeSeries ts : original_series){
+            System.out.println(ts);
         }
 
+        File original = new File("original.csv");
+        ICsvBeanWriter csvWriter  = new CsvBeanWriter(new FileWriter(original), CsvPreference.STANDARD_PREFERENCE);
+        String[] csvHeaderOriginal = {"measurementId","sensorId","measurementDate","measurementValue"};
+        String[] csvHeaderAggregation = {"lowDate","highDate","aggregationValue"};
+        List<File> files = new ArrayList<>();
+        csvWriter.writeHeader(csvHeaderOriginal);
+        for(TimeSeries ts : original_series){
+            csvWriter.write(ts,csvHeaderOriginal);
+        }
         csvWriter.close();
+        files.add(original);
+        for(String x : aggregations.keySet()){
+            File file = new File(x+".csv");
+            ICsvBeanWriter tmpWriter  = new CsvBeanWriter(new FileWriter(file), CsvPreference.STANDARD_PREFERENCE);
+            tmpWriter.writeHeader(csvHeaderAggregation);
+            for(Aggregation<Double> agg : aggregations.get(x)){
+                tmpWriter.write(agg,csvHeaderAggregation);
+            }
+            tmpWriter.close();
+            files.add(file);
+        }
+        return files;
+    }
 
+    @PostMapping("/downloadZip")
+    public void downloadFile(HttpServletResponse response , @RequestBody TimeSeriesRequestWithAggregations request) throws IOException {
+
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment;filename=download.zip");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        List<File> files = generateCSVFiles(request);
+
+        try (ZipOutputStream zippedOut = new ZipOutputStream(response.getOutputStream())) {
+            for (File file : files) {
+                FileSystemResource resource = new FileSystemResource(file);
+
+                ZipEntry e = new ZipEntry(resource.getFilename());
+                // Configure the zip entry, the properties of the file
+                e.setSize(resource.contentLength());
+                e.setTime(System.currentTimeMillis());
+                // etc.
+                zippedOut.putNextEntry(e);
+                // And the content of the resource:
+                StreamUtils.copy(resource.getInputStream(), zippedOut);
+                zippedOut.closeEntry();
+            }
+            zippedOut.finish();
+        } catch (Exception e) {
+            // Exception handling goes here
+        }
     }
 
 }
